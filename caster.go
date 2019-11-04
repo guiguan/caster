3736c4464f38ddd5f75b213cf456c979445d9806
Guan Gui <root@guiguan.net>
2019-11-04 16:18:07 +1100

    Makes `sub` returns a closed channel instead of a nil channel when ok is false

--- caster.go
+++ caster.go
@@ -2,7 +2,7 @@
  * @Author: guiguan
  * @Date:   2019-09-19T00:53:54+10:00
  * @Last modified by:   guiguan
- * @Last modified time: 2019-09-20T01:03:49+10:00
+ * @Last modified time: 2019-11-04T16:17:09+11:00
  */
 
 // Package caster implements a dead simple and performant message broadcaster (pubsub) library
@@ -119,8 +119,9 @@ func (c *Caster) Done() <-chan struct{} {
 	return c.done
 }
 
-// Close closes current caster and all subscriber channels. Ok value indicates whether current
-// caster has already been closed and stopped receiving further operations.
+// Close closes current caster and all subscriber channels. Ok value indicates whether the operation
+// is performed or not. When current caster is closed, it stops receiving further operations and the
+// operation won't be performed.
 func (c *Caster) Close() (ok bool) {
 	select {
 	case <-c.done:
@@ -132,10 +133,11 @@ func (c *Caster) Close() (ok bool) {
 	}
 }
 
-// Sub subscribes to current caster and returns a new channel for the subscriber to receive the
-// broadcasting message. When the given ctx is canceled, current caster will unsubscribe the
-// subscriber channel and close it. Ok value indicates whether current caster has already
-// been closed and stopped receiving further operations.
+// Sub subscribes to current caster and returns a new channel with the given buffer for the
+// subscriber to receive the broadcasting message. When the given ctx is canceled, current caster
+// will unsubscribe the subscriber channel and close it. Ok value indicates whether the operation is
+// performed or not. When current caster is closed, it stops receiving further operations and the
+// operation won't be performed. A closed receiver channel will be returned if ok is false.
 func (c *Caster) Sub(ctx context.Context, capacity uint) (sCh chan interface{}, ok bool) {
 	if ctx == nil {
 		ctx = context.Background()
@@ -145,9 +147,9 @@ func (c *Caster) Sub(ctx context.Context, capacity uint) (sCh chan interface{}, 
 
 	select {
 	case <-ctx.Done():
-		return nil, false
+		close(sCh)
 	case <-c.done:
-		return nil, false
+		close(sCh)
 	case c.op <- operation{
 		operator: opSub,
 		operand: subInfo{
@@ -155,12 +157,15 @@ func (c *Caster) Sub(ctx context.Context, capacity uint) (sCh chan interface{}, 
 			ctx: ctx,
 		},
 	}:
-		return sCh, true
 	}
+
+	ok = true
+	return
 }
 
 // Unsub unsubscribes the given subscriber channel from current caster and closes it. Ok value
-// indicates whether current caster has already been closed and stopped receiving further operations.
+// indicates whether the operation is performed or not. When current caster is closed, it stops
+// receiving further operations and the operation won't be performed.
 func (c *Caster) Unsub(subCh chan interface{}) (ok bool) {
 	select {
 	case <-c.done:
@@ -174,8 +179,9 @@ func (c *Caster) Unsub(subCh chan interface{}) (ok bool) {
 }
 
 // Pub publishes the given message to current caster, so the caster in turn broadcasts the message
-// to all subscriber channels. Ok value indicates whether current caster has already been closed and
-// stopped receiving further operations.
+// to all subscriber channels. Ok value indicates whether the operation is performed or not. When
+// current caster is closed, it stops receiving further operations and the operation won't be
+// performed.
 func (c *Caster) Pub(msg interface{}) (ok bool) {
 	select {
 	case <-c.done:
@@ -190,8 +196,8 @@ func (c *Caster) Pub(msg interface{}) (ok bool) {
 
 // TryPub publishes the given message to current caster, so the caster in turn broadcasts the
 // message to all subscriber channels without blocking on waiting for channels to be ready for
-// receiving. Ok value indicates whether current caster has already been closed and stopped
-// receiving further operations.
+// receiving. Ok value indicates whether the operation is performed or not. When current caster is
+// closed, it stops receiving further operations and the operation won't be performed.
 func (c *Caster) TryPub(msg interface{}) (ok bool) {
 	select {
 	case <-c.done:
